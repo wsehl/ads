700925e0bd1451f5786160b56037bef959a362cb
Alex Sehl <widesehl@gmail.com>
2023-04-05 11:17:29 +0600

    refactor: fibonacci

--- src/lib.rs
+++ src/lib.rs
@@ -53,12 +53,16 @@ pub mod recursion {
         }
     }
 
-    pub fn fibonacci(n: u64) -> u64 {
+    fn fibonacci_recursive(n: u64) -> u64 {
         match n {
             0 => 0,
             1 => 1,
             _ => fibonacci(n - 1) + fibonacci(n - 2),
         }
+    }
+
+    pub fn fibonacci(n: u64) -> u64 {
+        return fibonacci_recursive(n);
     }
 
     pub fn power(a: u64, n: u64) -> u64 {
