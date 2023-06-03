a8b4c178688fcb56c00c963ea5e2b73b658d7276
Alex Sehl <widesehl@gmail.com>
2023-06-03 16:45:15 +0600

    chore: init

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Assignment 3
+# Assignment 6
 
-HashTable implementation
+Graphs
--- src/App.java
+++ src/App.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class App {
     public static void main(String[] args) throws Exception {
 
