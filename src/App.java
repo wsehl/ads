ab7af520fca6b1e91aad01df39ad96868fcc6d06
Alex Sehl <widesehl@gmail.com>
2023-06-03 17:27:58 +0600

    feat: example

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
-public class App {
-    public static void main(String[] args) throws Exception {
 
+import example.*;
+
+public class App {
+    public static void outputPath(Search<String> search, String key) {
+        for (String v : search.pathTo(key)) {
+            System.out.print(v + " -> ");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        WeightedGraph<String> graph = new WeightedGraph<>(true);
+
+        graph.addEdge("Almaty", "Astana", 2.1);
+        graph.addEdge("Almaty", "Shymkent", 7.2);
+        graph.addEdge("Shymkent", "Astana", 3.9);
+        graph.addEdge("Astana", "Kostanay", 3.5);
+        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
+
+        System.out.println("Dijkstra:");
+        Search<String> djk = new DijkstraSearch<>(graph, "Almaty");
+        outputPath(djk, "Kyzylorda");
     }
 }
