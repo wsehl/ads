4ebb7dd34155e2ec1d5ecd6c3a38bdabe1a347a8
Alex Sehl <widesehl@gmail.com>
2023-06-03 20:46:36 +0600

    refactor: vertex

--- src/core/Vertex.java
+++ src/core/Vertex.java
@@ -5,11 +5,10 @@ import java.util.HashMap;
 
 public class Vertex<V> {
     private V data;
-    private Map<Vertex<V>, Double> adjacentVertices;
+    private Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
 
     public Vertex(V data) {
         this.data = data;
-        this.adjacentVertices = new HashMap<>();
     }
 
     public V getData() {
