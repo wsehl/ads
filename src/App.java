b15b3f9145905a065148c61d4216de03d251c246
Alex Sehl <widesehl@gmail.com>
2023-06-03 20:45:32 +0600

    refactor: path output

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 import core.WeightedGraph;
 import search.Search;
 import search.BreadthFirstSearch;
@@ -6,35 +8,41 @@ import search.DijkstraSearch;
 
 public class App {
     public static void outputPath(Search<String> search, String key) {
-        for (String v : search.pathTo(key)) {
-            System.out.print(v + " -> ");
+        Iterator<String> iterator = search.pathTo(key).iterator();
+
+        while (iterator.hasNext()) {
+            String v = iterator.next();
+            System.out.print(v);
+            if (iterator.hasNext()) {
+                System.out.print(" -> ");
+            }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        WeightedGraph<String> weightedGraph = new WeightedGraph<>(true);
+        WeightedGraph<String> graph = new WeightedGraph<>(true);
 
-        weightedGraph.addEdge("Almaty", "Astana", 2.1);
-        weightedGraph.addEdge("Almaty", "Shymkent", 7.2);
-        weightedGraph.addEdge("Shymkent", "Astana", 3.9);
-        weightedGraph.addEdge("Astana", "Kostanay", 3.5);
-        weightedGraph.addEdge("Shymkent", "Kyzylorda", 5.4);
+        graph.addEdge("Almaty", "Astana", 2.1);
+        graph.addEdge("Almaty", "Shymkent", 7.2);
+        graph.addEdge("Shymkent", "Astana", 3.9);
+        graph.addEdge("Astana", "Kostanay", 3.5);
+        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
 
         // Dijkstra
         System.out.println("Dijkstra:");
-        Search<String> djk = new DijkstraSearch<>(weightedGraph, "Almaty");
+        Search<String> djk = new DijkstraSearch<>(graph, "Almaty");
         outputPath(djk, "Kyzylorda");
         System.out.println("\n");
 
         // BFS
         System.out.println("Breadth First Search:");
-        Search<String> bfs = new BreadthFirstSearch<>(weightedGraph, "Almaty");
+        Search<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
         outputPath(bfs, "Kyzylorda");
         System.out.println("\n");
 
         // DFS
         System.out.println("Depth First Search:");
-        Search<String> dfs = new DepthFirstSearch<>(weightedGraph, "Almaty");
+        Search<String> dfs = new DepthFirstSearch<>(graph, "Almaty");
         outputPath(dfs, "Kyzylorda");
         System.out.println("\n");
     }
